f281547bf58aaf981f50d545e4478c2ff344a340
gns0170 <gns0170>
2021-12-03 16:28:55 +0900

    1st

---
+++ src/main/java/com/example/knumaptest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.knumaptest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main)
+
+        var btnLogin = findViewById(R.id.btnLogin) as Button
+        btnLogin.setOnClickListener ({
+            val intent = Intent(this, MainAct::class.java)
+            startActivity(intent)
+        })
+    }
+}
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
+
+class MainAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
