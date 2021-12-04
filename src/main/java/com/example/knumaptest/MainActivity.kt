d558acf320c272f09fe5e63e496564e014dbae6b
gns0170 <gns0170>
2021-12-04 18:38:08 +0900

    3rd

--- src/main/java/com/example/knumaptest/MainActivity.kt
+++ src/main/java/com/example/knumaptest/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.knumaptest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import android.widget.Button
-import androidx.core.content.ContextCompat.startActivity
 
 class TitleActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,30 +14,67 @@ class TitleActivity : AppCompatActivity(){
     }
 }
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
 
         var btnLogin = findViewById(R.id.btnLogin) as Button
         btnLogin.setOnClickListener ({
             val intent = Intent(this, MainAct::class.java)
             startActivity(intent)
+            finish()
         })
-    }
-}
-
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
     }
 }
 
 class MainAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main)
+
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        getSupportActionBar()?.setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar()?.setDisplayUseLogoEnabled(true);
+
+        var btnLogin = findViewById(R.id.testbtn) as Button
+        btnLogin.setOnClickListener ({
+            val intent = Intent(this, MainAct2::class.java)
+            startActivity(intent)
+            overridePendingTransition(0, 0);
+            finish()
+        })
+    }
+}
+
+class MainAct2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main2)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        getSupportActionBar()?.setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar()?.setDisplayUseLogoEnabled(true);
+
+        var btnLogin = findViewById(R.id.testbtn2) as Button
+        btnLogin.setOnClickListener ({
+            val intent = Intent(this, MainAct::class.java)
+            startActivity(intent)
+            overridePendingTransition(0, 0);
+            finish()
+        })
     }
 }
