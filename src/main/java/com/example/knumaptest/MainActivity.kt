08c6b6083a48e760642518efd3dc64ef2af6eb42
gns0170 <gns0170>
2021-12-06 02:06:27 +0900

    6th, connect pages with fragment and load listView Successful

--- src/main/java/com/example/knumaptest/MainActivity.kt
+++ src/main/java/com/example/knumaptest/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.knumaptest
 
+import android.app.Activity
+import android.app.PendingIntent.getActivity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ListView
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -23,22 +23,6 @@ class TitleActivity : AppCompatActivity(){
     }
 }
 
-
-//스플래시 스크린 -> 없어도 됌 -> 로그인 화면
-class MainActivity : AppCompatActivity() {
-
-    //  initialize viewpager
-    private val adapter by lazy { ViewPagerAdapter(supportFragmentManager) }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val viewPager_main = findViewById<ViewPager>(R.id.viewPager_main)
-        viewPager_main.adapter = MainActivity@adapter
-
-
-    }
-}
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,95 +31,64 @@ class LoginActivity : AppCompatActivity() {
     }
 }
 
+//스플래시 스크린 -> 없어도 됌 -> 로그인 화면
+class MainActivity : AppCompatActivity() {
 
-//메인화면 1, 수업과 시간표
-class MainAct : AppCompatActivity() {
+    //  initialize viewpager
+    private val adapter by lazy { ViewPagerAdapter(supportFragmentManager) }
 
-
-    var ContentList = arrayListOf<ListViewItem>(
-        ListViewItem("1A","운영관리", "융복합관", "7:00 - 9:00")
-
-    )
+    //1,2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
 
-        ContentList.add(ListViewItem("2A", "자바 프로그래밍", "IT5", "10:30 - 12:00"))
-        ContentList.add(ListViewItem("3A", "자료 구조", "IT4", "1:30 - 3:00"))
-        val classAdapter = ListViewAdapter(this, ContentList)
-
-        val classlist = findViewById<ListView>(R.id.classlist)
-        classlist.adapter = classAdapter
-
+        setContentView(R.layout.activity_main)
+        val viewPager_main = findViewById<ViewPager>(R.id.viewPager_main)
+        viewPager_main.adapter = MainActivity@adapter
 
     }
+}
+
+//메인화면 1, 수업과 시간표
+class FirstFragment : Fragment() {
+    var ContentList = arrayListOf<ListViewItem>(
+            ListViewItem("1A", "운영관리", "융복합관", "7:00 - 9:00")
+    )
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.main, container, false)
+        ContentList.add(ListViewItem("2A", "자바 프로그래밍", "IT5", "10:30 - 12:00"))
+        ContentList.add(ListViewItem("3A", "자료 구조", "IT4", "1:30 - 3:00"))
+        val classAdapter = getActivity()?.let { ListViewAdapter(it, ContentList) }
+        val classlist = view.findViewById<ListView>(R.id.classlist)
+        classlist.adapter = classAdapter
+        return view
+    }
+
 }
 
 //메인화면 2, 식단표
-class MainAct2 : AppCompatActivity() {
-
-    var ContentList = arrayListOf<Rest_LVItem>(
-            Rest_LVItem("복지관 식당","11:00 - 13:00", "복지관", "복지관 치즈 돈가스")
+class SecondFragment : Fragment(){
+    var ContentListRest = arrayListOf<Rest_LVItem>(
+            Rest_LVItem("복지관 식당", "11:00 - 13:00", "복지관", "복지관 치즈 돈가스")
     )
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        setContentView(R.layout.main2)
-        ContentList.add(Rest_LVItem("첨성관 식당", "11:00 - 13:00", "첨성관", "-  김치찌개\n-  흰 쌀밥\n-  김치\n-  시금치 무침\n-  불고기"))
-        val classAdapter = Rest_LVAdapter(this, ContentList)
-
-        val restList = findViewById<ListView>(R.id.restList)
-        restList.adapter = classAdapter
-
-
-
-
+        val view = inflater.inflate(R.layout.main2, container, false)
+        ContentListRest.add(Rest_LVItem("첨성관 식당", "11:00 - 13:00", "첨성관", "-  김치찌개\n-  흰 쌀밥\n-  김치\n-  시금치 무침\n-  불고기"))
+        val restAdapter = getActivity()?.let{Rest_LVAdapter(it, ContentListRest)}
+        val restList = view.findViewById<ListView>(R.id.restList)
+        restList.adapter = restAdapter
+        return view
     }
 }
-
-
-
 
 //페이지 전환 애니메이션
-class FirstFragment : Fragment(){
-
-
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.main, container, false)
-
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-}
-class SecondFragment : Fragment(){
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.main2, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-}
-
 class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when(position) {
             0 -> FirstFragment()
-            else       ->  SecondFragment()
+            else  ->  SecondFragment()
         }
     }
 
